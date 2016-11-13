dfeb3be976230fe08bb8df265fa5dc51246a724a
JoaoGabrielSG <joaogabriel@MacBook-Pro-de-joao.local>
2016-11-13 19:41:49 -0300

    initial commit

---
+++ Lista02_01/Lista02_01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lista02_01
+//
+//  Created by joão gabriel on 13/11/16.
+//  Copyright © 2016 bepid. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
