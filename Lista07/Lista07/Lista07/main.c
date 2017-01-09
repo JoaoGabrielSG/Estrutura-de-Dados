5eada32f0791955ad562a17c5326774437d51e27
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-01-09 19:40:10 -0300

    add list 07

---
+++ Lista07/Lista07/Lista07/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lista07
+//
+//  Created by joão gabriel on 09/01/17.
+//  Copyright © 2017 bepid. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
